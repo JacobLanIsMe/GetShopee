cb57c2f4fc750fde953762a629e457f806cc672b
紹瑋 藍 <maimaisatt@gmail.com>
2022-09-14 01:40:21 +0800

    加入專案檔案。

---
+++ prjGetShopee/GetShopee.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace prjGetShopee
+{
+    partial class GetShopee
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetProduct = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnGetProduct
+            // 
+            this.btnGetProduct.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGetProduct.Location = new System.Drawing.Point(12, 365);
+            this.btnGetProduct.Name = "btnGetProduct";
+            this.btnGetProduct.Size = new System.Drawing.Size(230, 97);
+            this.btnGetProduct.TabIndex = 0;
+            this.btnGetProduct.Text = "取得商品資訊";
+            this.btnGetProduct.UseVisualStyleBackColor = true;
+            this.btnGetProduct.Click += new System.EventHandler(this.btnGetProduct_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 40;
+            this.listBox1.Location = new System.Drawing.Point(266, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1002, 604);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnTest.Location = new System.Drawing.Point(12, 550);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(230, 97);
+            this.btnTest.TabIndex = 4;
+            this.btnTest.Text = "測試";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_ClickAsync);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(1291, 57);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(478, 604);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // GetShopee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1816, 986);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGetProduct);
+            this.Name = "GetShopee";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetProduct;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ prjGetShopee/GetShopee.cs
@@ -0,0 +1,94 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjGetShopee
+{
+    public partial class GetShopee : Form
+    {
+        public GetShopee()
+        {
+            InitializeComponent();
+        }
+        List<string> bigTypeUrl = new List<string>();
+        Random random = new Random();
+        private async void btnGetProduct_Click(object sender, EventArgs e)
+        {
+            EdgeOptions edgeoptions = new EdgeOptions();
+            edgeoptions.PageLoadStrategy = PageLoadStrategy.Normal;
+            EdgeDriver driver = new EdgeDriver(edgeoptions);
+            driver.Navigate().GoToUrl("https://shopee.tw/all_categories");
+            var bigType = driver.FindElements(By.CssSelector(".category-grid"));
+            foreach (var i in bigType)
+            {
+                string bigTypeName = i.Text;
+                if (bigTypeName == "其他類別") continue;
+                bigTypeUrl.Add(i.GetAttribute("href"));
+            }
+            foreach (var i in bigTypeUrl)
+            {
+                driver.Navigate().GoToUrl(i);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                string bigTypeName = driver.FindElement(By.CssSelector("a.shopee-category-list__main-category__link")).Text;
+                List<string> smallTypeUrl = new List<string>();
+                List<string> smallTypeName = new List<string>();
+                driver.FindElement(By.CssSelector("div.shopee-category-list__toggle-btn")).Click();
+                var smallType = driver.FindElements(By.CssSelector("a.shopee-category-list__sub-category"));
+                foreach (var j in smallType)
+                {
+                    smallTypeUrl.Add(j.GetAttribute("href"));
+                    smallTypeName.Add(j.Text);
+                }
+                for (int j = 0; j<smallTypeUrl.Count;j++)
+                {
+                    
+                    driver.Navigate().GoToUrl(smallTypeUrl[j]);
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    driver.FindElement(By.CssSelector("div.shopee-sort-by-options>div:nth-child(2)")).Click();
+                    int productCount = random.Next(10, 30);
+                    for (int k = 1; k <= productCount; k++)
+                    {
+                        driver.FindElement(By.XPath($"//*[@id='main']/div/div[2]/div/div/div[3]/div[2]/div/div[2]/div[{k}]/a")).Click();
+                        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                        string productName = driver.FindElement(By.CssSelector("._2rQP1z>span")).Text;
+                        List<byte[]> productPhotos = await GetProductPhotos(driver);
+
+                        driver.Navigate().Back();
+                    }
+                    
+                }
+            }
+            
+            driver.Quit();
+        }
+        private async Task<List<byte[]>> GetProductPhotos(EdgeDriver driver)
+        {
+            driver.FindElement(By.CssSelector("div.xK9doz")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            var photos = driver.FindElements(By.CssSelector("div.xK9doz div._1OPdfl>div"));
+            List<byte[]> productPhotos = new List<byte[]>();
+            foreach (var i in photos)
+            {
+                string photoUrl = i.GetAttribute("style").Split('"')[1];
+                HttpClient client = new HttpClient();
+                byte[] photo = await client.GetByteArrayAsync(photoUrl);
+                productPhotos.Add(photo);
+            }
+            return productPhotos;
+        }
+        private async void btnTest_ClickAsync(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
