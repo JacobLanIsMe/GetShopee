0c48cd7206d156b06dfe8e1a68ee317ddfc2d411
紹瑋 藍 <maimaisatt@gmail.com>
2022-09-14 17:31:36 +0800

    20220914_Add MemberAccount

--- prjGetShopee/GetShopee.Designer.cs
+++ prjGetShopee/GetShopee.Designer.cs
@@ -35,14 +35,19 @@ namespace prjGetShopee
             this.label2 = new System.Windows.Forms.Label();
             this.btnTest = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnClearDB = new System.Windows.Forms.Button();
+            this.btnAddMember = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGetProduct
             // 
             this.btnGetProduct.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnGetProduct.Location = new System.Drawing.Point(12, 365);
+            this.btnGetProduct.Location = new System.Drawing.Point(6, 182);
+            this.btnGetProduct.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.btnGetProduct.Name = "btnGetProduct";
-            this.btnGetProduct.Size = new System.Drawing.Size(230, 97);
+            this.btnGetProduct.Size = new System.Drawing.Size(106, 48);
             this.btnGetProduct.TabIndex = 0;
             this.btnGetProduct.Text = "取得商品資訊";
             this.btnGetProduct.UseVisualStyleBackColor = true;
@@ -52,36 +57,40 @@ namespace prjGetShopee
             // 
             this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 40;
-            this.listBox1.Location = new System.Drawing.Point(266, 57);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(123, 28);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1002, 604);
+            this.listBox1.Size = new System.Drawing.Size(465, 304);
             this.listBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(163, 134);
+            this.label1.Location = new System.Drawing.Point(75, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.Size = new System.Drawing.Size(33, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(155, 57);
+            this.label2.Location = new System.Drawing.Point(72, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.Size = new System.Drawing.Size(33, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
             // btnTest
             // 
             this.btnTest.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnTest.Location = new System.Drawing.Point(12, 550);
+            this.btnTest.Location = new System.Drawing.Point(6, 254);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(230, 97);
+            this.btnTest.Size = new System.Drawing.Size(106, 48);
             this.btnTest.TabIndex = 4;
             this.btnTest.Text = "測試";
             this.btnTest.UseVisualStyleBackColor = true;
@@ -89,24 +98,65 @@ namespace prjGetShopee
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(1291, 57);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(596, 28);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(478, 604);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(221, 302);
             this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // btnClearDB
+            // 
+            this.btnClearDB.BackColor = System.Drawing.Color.Red;
+            this.btnClearDB.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnClearDB.ForeColor = System.Drawing.Color.White;
+            this.btnClearDB.Location = new System.Drawing.Point(6, 434);
+            this.btnClearDB.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btnClearDB.Name = "btnClearDB";
+            this.btnClearDB.Size = new System.Drawing.Size(78, 34);
+            this.btnClearDB.TabIndex = 6;
+            this.btnClearDB.Text = "清除資料庫";
+            this.btnClearDB.UseVisualStyleBackColor = false;
+            this.btnClearDB.Click += new System.EventHandler(this.btnClearDB_Click);
+            // 
+            // btnAddMember
+            // 
+            this.btnAddMember.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAddMember.Location = new System.Drawing.Point(6, 337);
+            this.btnAddMember.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.btnAddMember.Name = "btnAddMember";
+            this.btnAddMember.Size = new System.Drawing.Size(106, 48);
+            this.btnAddMember.TabIndex = 7;
+            this.btnAddMember.Text = "新增會員";
+            this.btnAddMember.UseVisualStyleBackColor = true;
+            this.btnAddMember.Click += new System.EventHandler(this.btnAddMember_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(123, 364);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(694, 265);
+            this.dataGridView1.TabIndex = 8;
             // 
             // GetShopee
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1816, 986);
+            this.ClientSize = new System.Drawing.Size(1151, 735);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAddMember);
+            this.Controls.Add(this.btnClearDB);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnGetProduct);
+            this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.Name = "GetShopee";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +170,9 @@ namespace prjGetShopee
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnTest;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnClearDB;
+        private System.Windows.Forms.Button btnAddMember;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- prjGetShopee/GetShopee.cs
+++ prjGetShopee/GetShopee.cs
@@ -1,5 +1,7 @@
-﻿using OpenQA.Selenium;
+﻿using ExcelDataReader;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,13 +22,15 @@ namespace prjGetShopee
         {
             InitializeComponent();
         }
+        iSpanProjectEntities dbContext = new iSpanProjectEntities();
         List<string> bigTypeUrl = new List<string>();
         Random random = new Random();
-        private async void btnGetProduct_Click(object sender, EventArgs e)
+        private void btnGetProduct_Click(object sender, EventArgs e)
         {
             EdgeOptions edgeoptions = new EdgeOptions();
             edgeoptions.PageLoadStrategy = PageLoadStrategy.Normal;
             EdgeDriver driver = new EdgeDriver(edgeoptions);
+            driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://shopee.tw/all_categories");
             var bigType = driver.FindElements(By.CssSelector(".category-grid"));
             foreach (var i in bigType)
@@ -40,6 +44,7 @@ namespace prjGetShopee
                 driver.Navigate().GoToUrl(i);
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                 string bigTypeName = driver.FindElement(By.CssSelector("a.shopee-category-list__main-category__link")).Text;
+                listBox1.Items.Add(bigTypeName);
                 List<string> smallTypeUrl = new List<string>();
                 List<string> smallTypeName = new List<string>();
                 driver.FindElement(By.CssSelector("div.shopee-category-list__toggle-btn")).Click();
@@ -51,21 +56,25 @@ namespace prjGetShopee
                 }
                 for (int j = 0; j<smallTypeUrl.Count;j++)
                 {
-                    
+                    listBox1.Items.Add($"    {smallTypeName[j]}");
                     driver.Navigate().GoToUrl(smallTypeUrl[j]);
                     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                     driver.FindElement(By.CssSelector("div.shopee-sort-by-options>div:nth-child(2)")).Click();
-                    int productCount = random.Next(10, 30);
+                    //var moveToEle = driver.FindElement(By.CssSelector("div.shopee-page-controller"));
+                    //Actions action = new Actions(driver);
+                    //action.MoveToElement(moveToEle).Perform();
+                    //int productTotalCount = driver.FindElements(By.CssSelector("div.shopee-search-item-result__item>a")).ToList().Count;
+                    //listBox1.Items.Add($"        {productTotalCount}");
+                    int productCount = random.Next(5, 15+1);
                     for (int k = 1; k <= productCount; k++)
                     {
                         driver.FindElement(By.XPath($"//*[@id='main']/div/div[2]/div/div/div[3]/div[2]/div/div[2]/div[{k}]/a")).Click();
                         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                         string productName = driver.FindElement(By.CssSelector("._2rQP1z>span")).Text;
-                        List<byte[]> productPhotos = await GetProductPhotos(driver);
-
+                        listBox1.Items.Add($"        {productName}");
                         driver.Navigate().Back();
                     }
-                    
+
                 }
             }
             
@@ -88,7 +97,146 @@ namespace prjGetShopee
         }
         private async void btnTest_ClickAsync(object sender, EventArgs e)
         {
-            
+            var q = dbContext.MemberAccounts.Select(i => i.MemPic);
+            foreach (var i in q)
+            {
+                MemoryStream ms = new MemoryStream(i);
+                PictureBox pb = new PictureBox();
+                pb.Image = Image.FromStream(ms);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                flowLayoutPanel1.Controls.Add(pb);
+            }
         }
+
+        private void btnClearDB_Click(object sender, EventArgs e)
+        {
+            var comments = dbContext.Comments.Select(i => i);
+            foreach(var i in comments)
+            {
+                dbContext.Comments.Remove(i);
+            }
+            var follows = dbContext.Follows.Select(i => i);
+            foreach (var i in follows)
+            {
+                dbContext.Follows.Remove(i);
+            }
+            var likes = dbContext.Likes.Select(i => i);
+            foreach (var i in likes)
+            {
+                dbContext.Likes.Remove(i);
+            }
+            var officialCoupons = dbContext.OfficialCoupons.Select(i => i);
+            foreach (var i in officialCoupons)
+            {
+                dbContext.OfficialCoupons.Remove(i);
+            }
+            var orderDetails = dbContext.OrderDetails.Select(i => i);
+            foreach (var i in orderDetails)
+            {
+                dbContext.OrderDetails.Remove(i);
+            }
+            var orders = dbContext.Orders.Select(i => i);
+            foreach (var i in orders)
+            {
+                dbContext.Orders.Remove(i);
+            }
+            var productDetails = dbContext.ProductDetails.Select(i => i);
+            foreach (var i in productDetails)
+            {
+                dbContext.ProductDetails.Remove(i);
+            }
+            var productPics = dbContext.ProductPics.Select(i => i);
+            foreach (var i in productPics)
+            {
+                dbContext.ProductPics.Remove(i);
+            }
+            var shipperToProducts = dbContext.ShipperToProducts.Select(i => i);
+            foreach (var i in shipperToProducts)
+            {
+                dbContext.ShipperToProducts.Remove(i);
+            }
+            var products = dbContext.Products.Select(i => i);
+            foreach (var i in products)
+            {
+                dbContext.Products.Remove(i);
+            }
+            var smallTypes = dbContext.SmallTypes.Select(i => i);
+            foreach (var i in smallTypes)
+            {
+                dbContext.SmallTypes.Remove(i);
+            }
+            var bigTypes = dbContext.BigTypes.Select(i => i);
+            foreach (var i in bigTypes)
+            {
+                dbContext.BigTypes.Remove(i);
+            }
+            var memberAccounts = dbContext.MemberAccounts.Select(i => i);
+            foreach (var i in memberAccounts)
+            {
+                dbContext.MemberAccounts.Remove(i);
+            }
+            dbContext.SaveChanges();
+            MessageBox.Show("清除完成");
+
+        }
+
+        private async void btnAddMember_Click(object sender, EventArgs e)
+        {
+            FileStream fs = File.Open("memberInfo.xlsx", FileMode.Open, FileAccess.Read);
+            var reader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+
+            DataSet ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+            DataTable dt = ds.Tables["員工資料1"];
+            
+            Random random = new Random();
+            DateTime startDate = new DateTime(1950, 1, 1);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string countryName = dt.Rows[i]["Country"].ToString();
+                int countryID = dbContext.CountryLists.Where(k => k.CountryName == countryName).Select(k => k.CountryID).FirstOrDefault();
+                string regionName = dt.Rows[i]["Region"].ToString();
+                int regionID = dbContext.RegionLists.Where(k => k.RegionName == regionName && k.CountryID == countryID).Select(k => k.RegionID).FirstOrDefault();
+                string phone = "09" + random.Next(1, 99999999).ToString("00000000");
+                string email = dt.Rows[i]["登入帳號"].ToString();
+                string address = dt.Rows[i]["地址"].ToString();
+                string name = dt.Rows[i]["員工姓名"].ToString();
+                int dateRange = (DateTime.Today - startDate).Days;
+                DateTime birthday = startDate.AddDays(random.Next(dateRange));
+                string bio = dt.Rows[i]["所屬區域"].ToString() + dt.Rows[i]["部門"].ToString() + dt.Rows[i]["職稱"].ToString();
+                HttpClient client = new HttpClient();
+                string photoUrl = dt.Rows[i]["照片"].ToString();
+                byte[] photo = await client.GetByteArrayAsync(photoUrl);
+                MemberAccount member = new MemberAccount
+                {
+                    MemberAcc = $"Acc{i + 1}",
+                    MemberPw = $"Pw{i + 1}",
+                    TWorNOT = true,
+                    RegionID = regionID,
+                    Phone = phone,
+                    Email = email,
+                    Address = address,
+                    Name = name,
+                    Birthday = birthday,
+                    Bio = bio,
+                    MemPic = photo,
+                    MemStatusID = 1
+                };
+                dbContext.MemberAccounts.Add(member);
+                dbContext.SaveChanges();
+            }
+            MessageBox.Show("新增會員完成");
+            dataGridView1.DataSource = ds.Tables["員工資料1"];
+
+            
+           
+        }
+
+       
     }
 }
